b90acc0732c96f4fc6f2aa37b5fa007e58f190d0
Vijay Shankar <vijayayush.ak15@gmail.com>
2022-10-08 19:33:34 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.
+    i'm going to create my first react app</p>
+  </>
+);
