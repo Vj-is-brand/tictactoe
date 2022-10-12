749078d3b384f190813f0e8ec20bd7f33f1e4e77
Vijay Shankar <vijayayush.ak15@gmail.com>
2022-10-12 21:03:47 +0530

    calculate winner and lift up the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./component/Board";
+import { calculateWinner } from "./helper";
 
 import "./styles/root.scss"
 
 const App = () => {
+
+  const [ board, setboard ] = useState(Array(9).fill(null));
+const [ isXNext , setIsXNext ] = useState(false);
+
+const winner = calculateWinner(board);
+const Message = winner ? `winner is ${winner} ` : `Next player is ${ isXNext ? 'x' : 'o' }` ;
+
+const handleSquareClick = (position) => {
+
+  if(board[position] || winner){
+   return;
+  }
+
+  setboard( (prev)=> {
+   return prev.map((Square, pos ) => {
+    if(pos === position){
+      return isXNext ? 'x' : 'o';
+    }
+    return Square;
+   });
+  });
+ setIsXNext( prev => !prev);
+};
+
   return (
   <div className="app">
     <h1> TicTacToe</h1>
-        <Board/>
+    <h2>{ Message }</h2>
+        <Board board={board} handleSquareClick={handleSquareClick}/>
   </div>
 );
 };
